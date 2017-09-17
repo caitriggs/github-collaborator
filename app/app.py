f05d742b6434cf3630971fe605f0cf23bbdf6396
Cait <caitriggs@gmail.com>
2017-09-17 16:11:57 -0700

    Start off flask web app with hard coded examples

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from flask import Flask, request, render_template
+from werkzeug import secure_filename
+import pandas as pd
+import numpy as np
+import requests
+import sys
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/madrury')
+def get_madrury():
+    return render_template('madrury.html')
+
+@app.route('/torvalds')
+def get_torvalds():
+    return render_template('torvalds.html')
+
+if __name__ == '__main__':
+    app.run()
